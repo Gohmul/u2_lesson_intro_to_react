12d3f7e7de2427bf16f736815954e3567f959b51
Andre Pato <anpato1994@gmail.com>
2021-08-22 01:30:02 -0400

    update starter code

--- scripts/button.js
+++ scripts/button.js
@@ -1,18 +1,1 @@
 const container = document.getElementById('root')
-
-function Button() {
-  const [counter, updateCounter] = React.useState(0)
-
-  return React.createElement(
-    'button',
-    {
-      onClick: () => updateCounter(counter + 1),
-      className: 'my-button'
-    },
-    counter
-  )
-}
-
-const element = React.createElement(Button)
-
-ReactDOM.render(element, container)
